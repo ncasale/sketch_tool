f6d212d47cb2f44d7cc6b6bf53a40bb4524d23e0
Nathan Casale <Nathan Casale>
2017-02-28 13:06:37 -0500

    Added screen splits

--- SketchTool/ui_mainwindow.h
+++ SketchTool/ui_mainwindow.h
@@ -13,13 +13,17 @@
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPlainTextEdit>
+#include <QtWidgets/QSplitter>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QToolBar>
 #include <QtWidgets/QVBoxLayout>
+#include <QtWidgets/QWidget>
 #include <myglwidget.h>
 
 QT_BEGIN_NAMESPACE
@@ -32,19 +36,27 @@ public:
     QAction *actionClear;
     QAction *actionOpen;
     QAction *actionSave_As;
-    MyGLWidget *centralWidget;
+    QWidget *centralWidget;
     QVBoxLayout *verticalLayout;
+    QSplitter *splitter;
+    QWidget *widget;
+    QHBoxLayout *horizontalLayout;
+    QSplitter *splitter_2;
+    MyGLWidget *widget_2;
+    QWidget *widget_3;
+    QPlainTextEdit *plainTextEdit;
     QMenuBar *menuBar;
     QMenu *menuFile;
     QToolBar *mainToolBar;
     QStatusBar *statusBar;
     QToolBar *toolBar;
+    QToolBar *toolBar_2;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QStringLiteral("MainWindow"));
-        MainWindow->resize(800, 800);
+        MainWindow->resize(779, 694);
         MainWindow->setFocusPolicy(Qt::StrongFocus);
         actionClose = new QAction(MainWindow);
         actionClose->setObjectName(QStringLiteral("actionClose"));
@@ -65,16 +77,65 @@ public:
         QIcon icon2;
         icon2.addFile(QStringLiteral(":/Images/toolbar_icons/save_as.png"), QSize(), QIcon::Normal, QIcon::Off);
         actionSave_As->setIcon(icon2);
-        centralWidget = new MyGLWidget(MainWindow);
+        centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QStringLiteral("centralWidget"));
         verticalLayout = new QVBoxLayout(centralWidget);
         verticalLayout->setSpacing(6);
         verticalLayout->setContentsMargins(11, 11, 11, 11);
         verticalLayout->setObjectName(QStringLiteral("verticalLayout"));
+        splitter = new QSplitter(centralWidget);
+        splitter->setObjectName(QStringLiteral("splitter"));
+        QSizePolicy sizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::MinimumExpanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(splitter->sizePolicy().hasHeightForWidth());
+        splitter->setSizePolicy(sizePolicy);
+        splitter->setOrientation(Qt::Vertical);
+        widget = new QWidget(splitter);
+        widget->setObjectName(QStringLiteral("widget"));
+        horizontalLayout = new QHBoxLayout(widget);
+        horizontalLayout->setSpacing(6);
+        horizontalLayout->setContentsMargins(11, 11, 11, 11);
+        horizontalLayout->setObjectName(QStringLiteral("horizontalLayout"));
+        splitter_2 = new QSplitter(widget);
+        splitter_2->setObjectName(QStringLiteral("splitter_2"));
+        splitter_2->setOrientation(Qt::Horizontal);
+        widget_2 = new MyGLWidget(splitter_2);
+        widget_2->setObjectName(QStringLiteral("widget_2"));
+        QSizePolicy sizePolicy1(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy1.setHorizontalStretch(2);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(widget_2->sizePolicy().hasHeightForWidth());
+        widget_2->setSizePolicy(sizePolicy1);
+        splitter_2->addWidget(widget_2);
+        widget_3 = new QWidget(splitter_2);
+        widget_3->setObjectName(QStringLiteral("widget_3"));
+        QSizePolicy sizePolicy2(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy2.setHorizontalStretch(1);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(widget_3->sizePolicy().hasHeightForWidth());
+        widget_3->setSizePolicy(sizePolicy2);
+        splitter_2->addWidget(widget_3);
+
+        horizontalLayout->addWidget(splitter_2);
+
+        splitter->addWidget(widget);
+        plainTextEdit = new QPlainTextEdit(splitter);
+        plainTextEdit->setObjectName(QStringLiteral("plainTextEdit"));
+        QSizePolicy sizePolicy3(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy3.setHorizontalStretch(0);
+        sizePolicy3.setVerticalStretch(0);
+        sizePolicy3.setHeightForWidth(plainTextEdit->sizePolicy().hasHeightForWidth());
+        plainTextEdit->setSizePolicy(sizePolicy3);
+        plainTextEdit->setMaximumSize(QSize(16777215, 100));
+        splitter->addWidget(plainTextEdit);
+
+        verticalLayout->addWidget(splitter);
+
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QStringLiteral("menuBar"));
-        menuBar->setGeometry(QRect(0, 0, 800, 17));
+        menuBar->setGeometry(QRect(0, 0, 779, 17));
         menuFile = new QMenu(menuBar);
         menuFile->setObjectName(QStringLiteral("menuFile"));
         menuFile->setFocusPolicy(Qt::StrongFocus);
@@ -88,6 +149,9 @@ public:
         toolBar = new QToolBar(MainWindow);
         toolBar->setObjectName(QStringLiteral("toolBar"));
         MainWindow->addToolBar(Qt::TopToolBarArea, toolBar);
+        toolBar_2 = new QToolBar(MainWindow);
+        toolBar_2->setObjectName(QStringLiteral("toolBar_2"));
+        MainWindow->addToolBar(Qt::TopToolBarArea, toolBar_2);
 
         menuBar->addAction(menuFile->menuAction());
         menuFile->addAction(actionSave);
@@ -114,6 +178,7 @@ public:
         actionSave_As->setText(QApplication::translate("MainWindow", "Save As", 0));
         menuFile->setTitle(QApplication::translate("MainWindow", "File", 0));
         toolBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", 0));
+        toolBar_2->setWindowTitle(QApplication::translate("MainWindow", "toolBar_2", 0));
     } // retranslateUi
 
 };
